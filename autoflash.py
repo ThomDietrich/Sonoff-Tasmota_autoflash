f243bfe05ab9ce116479d49086a444468b208d4b
yann25 <home.doersam@web.de>
2017-03-12 21:46:01 +0100

    First commit

---
+++ autoflash.py
@@ -0,0 +1,54 @@
+import pip
+import os
+import sys
+import serial.tools.list_ports
+
+def install(package):
+    pip.main(['install', package])
+
+def ports():
+    ports = list(serial.tools.list_ports.comports())
+    for p in ports:
+        print p
+
+
+#user input
+print("Please connect your device now")
+print("Aviable Ports:")
+ports()
+
+port=raw_input("Which port do you want to use?\n")
+device= raw_input("Which device are you using? Type 0 for touch or 4CH and 1 for the others\n")
+filepath=raw_input("Enter the filepath of the firmwarefile\n")
+
+#install esptools
+install("esptool")
+
+# clear flash
+err_erase=os.system("esptool.py --port "+port+" erase_flash")
+print(err_erase)
+
+#Process Error
+if(err_erase !=0):
+    print("Error while erasing. Check your connection.")
+    sys.exit()
+
+
+#Wait to reconnect
+raw_input("Reconnect then press enter\n")
+
+#Select device
+if(device == "0"):
+#Flash with command for 4CH and Touch
+    err_write=os.system("esptool.py --port "+port+" write_flash -fm dout -fs detect 0x0 "+filepath)
+elif(device == "1"):
+#Flash with command for all others
+    err_write=os.system("esptool.py --port "+port+" write_flash -fm dio -fs detect 0x0 "+filepath)
+
+#Process Error
+if(err_write !=0):
+    print("Error while writing. Check your connection.")
+    sys.exit()
+
+print("Successful")
+#
