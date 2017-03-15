ecfde21eeef67d2fd43638957ccca404acb958ec
yann25 <home.doersam@web.de>
2017-03-15 21:39:46 +0100

    Add install dependencies

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from distutils.core import setup
 setup(
   name = 'sonoff_autoflash',
-  packages = ['sonoff_autoflash'], # this must be the same as the name above
-  version = 'v0.1',
+  packages = ['sonoff_autoflash'],
+  install_requires=[
+          'esptool',
+      ],
+  version = 'v0.1.2',
   description = 'little python script to flash sonoff devices',
   author = 'Yann Doersam',
   author_email = 'yann.doersam@gmail.com',
   url = 'https://github.com/yann25/Sonoff-Tasmota_autoflash', # use the URL to the github repo
-  download_url = 'https://github.com/yann25/Sonoff-Tasmota_autoflash/archive/v0.1.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/yann25/Sonoff-Tasmota_autoflash/archive/v0.1.2.tar.gz', # I'll explain this in a second
   keywords = ['ESP8266', 'flashing', 'sonoff', 'itead'], # arbitrary keywords
   classifiers = [],
+  zip_safe=False
 )
