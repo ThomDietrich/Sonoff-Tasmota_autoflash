850bcaf58b3149bf8733a9f39ba710d4558bc41b
yann25 <home.doersam@web.de>
2017-03-15 22:10:41 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
-from distutils.core import setup
+from setuptools import setup
 setup(
-  name = 'sonoff_autoflash',
-  packages = ['sonoff_autoflash'],
-  install_requires=[
-          'esptool',
-      ],
+  name = 'Sonoff-Tasmota_autoflash',
+  packages = ['Sonoff-Tasmota_autoflash'],
+  install_requires='esptool',
   version = 'v0.1.2',
   description = 'little python script to flash sonoff devices',
   author = 'Yann Doersam',
